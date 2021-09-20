638ced84b2e0a683cd02113200e321cb8d2beac5
DESKTOP-OHD6DA9\USER <kjp29816265@gmail.com>
2021-09-20 17:02:47 +0900

    뺵키 대응 + 나가는 UI

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject backmenu = null;
+    private void Start()
+    {
+        backmenu.SetActive(false);
+    }
+    private void Update()
+    {
+        BackButton();
+    }
     public void StartButton()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    private void BackButton()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            backmenu.SetActive(true);
+        }
+    }
+
+    public void MenuQuit()
+    {
+        backmenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
