91fc1a9bc8fe91ab2eacec58d1a6e90a3f201e66
DESKTOP-OHD6DA9\USER <kjp29816265@gmail.com>
2021-10-01 15:39:36 +0900

    간단한 이스터에그 추가, 설명창 버그 발생

--- Assets/Scripts/AnimationScene.cs
+++ Assets/Scripts/AnimationScene.cs
@@ -1,31 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class AnimationScene : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] scene = null;
+    private Image[] scene = null;
     [SerializeField]
     private float speed = 1f;
+
+    private void Awake()
+    {
+        SceneColorSetting();
+    }
     void Start()
     {
         StartCoroutine(Animation());
     }
 
+    private void SceneColorSetting()
+    {
+        for(int i = 0; i < scene.Length; i++)
+        {
+            if (i == 0) continue;
+            scene[i].color = new Color(1, 1, 1, 0);
+        }
+    }
+
     private IEnumerator Animation()
     {
-        yield return new WaitForSeconds(speed);
-        scene[1].SetActive(true);
-        yield return new WaitForSeconds(speed);
-        scene[2].SetActive(true);
-        yield return new WaitForSeconds(speed);
-        scene[3].SetActive(true);
-        yield return new WaitForSeconds(speed);
-        scene[4].SetActive(true);
-        yield return new WaitForSeconds(speed);
-        scene[5].SetActive(true);
+        for(int i = 0; i < scene.Length; i++)
+        {
+            yield return new WaitForSeconds(speed);
+            scene[i].gameObject.SetActive(true);
+            scene[i].DOColor(Color.white, 1.5f);
+        }
         yield return new WaitForSeconds(speed);
         SceneManager.LoadScene("Main");
     }
